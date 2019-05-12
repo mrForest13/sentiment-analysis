0e09991e97857fc7b68fa2a292e966b54d99c031
Mateusz Ligeza <mateusz.kamil.ligeza@gmail.com>
2019-05-12 17:52:39 +0200

    added multi domain loader

---
+++ experiment/IMDbExperiment.py
@@ -0,0 +1,78 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+
+from preprocessing.ProcessChainBuilder import ProcessChainBuilder
+from preprocessing.cleaning.NegationHandling import NegationHandlingProcessor
+from preprocessing.cleaning.Punctuations import PunctuationsProcessor
+from preprocessing.cleaning.StopWords import StopWordsProcessor
+from preprocessing.cleaning.TweeterHandling import TweeterHandlingProcessor
+from preprocessing.cleaning.FilterHtmlLink import FilterHtmlLinkProcessor
+from preprocessing.cleaning.FilterSpaces import FilterSpacesProcessor
+from preprocessing.cleaning.HtmlEncoding import HtmlEncodingProcessor
+from preprocessing.cleaning.LowercaseAll import LowercaseAllProcessor
+from preprocessing.cleaning.FilterAscii import FilterAsciiProcessor
+from preprocessing.normalization.DataLemmatizer import DataLemmatizer
+from preprocessing.normalization.TokenizerData import TokenizerData
+from preprocessing.normalization.JoinTokens import JoinTokens
+from classification.Classification import Classification, classifiers
+from sklearn.model_selection import train_test_split
+from loader.IMDbLoader import IMDbLoader
+from plot.Ploter import *
+
+data_loader = IMDbLoader("C:\\Users\\mateu\\Downloads\\aclImdb\\train", 'pos', 'neg')
+
+data_loader.load()
+
+data = data_loader.get_data()
+
+plot_horizontal_bar(data, data_loader.labels, 'Liczba tweetów')
+plot_box(data)
+
+data = ProcessChainBuilder() \
+    .next(LowercaseAllProcessor()) \
+    .next(FilterAsciiProcessor()) \
+    .next(FilterHtmlLinkProcessor()) \
+    .next(HtmlEncodingProcessor()) \
+    .next(TweeterHandlingProcessor()) \
+    .next(NegationHandlingProcessor()) \
+    .next(PunctuationsProcessor()) \
+    .next(StopWordsProcessor()) \
+    .next(FilterSpacesProcessor()) \
+    .build() \
+    .process(data)
+
+plot_word_cloud(data, 'negative')
+plot_word_cloud(data, 'positive')
+
+
+def split_data(frame, size=0.2):
+    return train_test_split(frame['text'], frame['sentiment'], test_size=size, stratify=frame['sentiment'])
+
+
+data = ProcessChainBuilder() \
+    .next(TokenizerData()) \
+    .next(DataLemmatizer()) \
+    .next(JoinTokens()) \
+    .build() \
+    .process(data)
+
+classification = Classification(repeats=3)
+
+train, test, train_labels, test_labels = split_data(data)
+
+cv = CountVectorizer(ngram_range=(1, 1))
+
+bag_of_words_train = cv.fit_transform(train)
+bag_of_words_test = cv.transform(test)
+
+classification.fit_all(bag_of_words_train, train_labels, scoring='accuracy')
+classification.predict_all(bag_of_words_test)
+
+best_accuracy = []
+
+for name, result in classification.predict_results.items():
+    acc = accuracy_score(test_labels, result)
+    best_accuracy.append(acc)
+    print("Accuracy for {}: {:.4%}".format(name, acc))
+
+plot_vertical_bar(best_accuracy, classifiers.keys(), 'Dokładność')
---
+++ loader/MultiDomainLoader.py
@@ -0,0 +1,45 @@
+import os
+import pandas
+
+from loader.Loader import Loader
+import lxml.etree as tree
+
+
+class MultiDomainLoader(Loader):
+
+    def __init__(self, data_directory, pos_file, neg_file):
+        super().__init__(['negative', 'positive'])
+        self.parser = tree.XMLParser(recover=True)
+        self.data_directory = data_directory
+        self.pos_file = pos_file
+        self.neg_file = neg_file
+
+    def load(self):
+        results = []
+        for category in self.list_dir():
+            print(category)
+            neg_data = self.load_data(category, self.neg_file, 'negative')
+            pos_data = self.load_data(category, self.pos_file, 'positive')
+            results = results + neg_data + pos_data
+            print(len(results))
+
+        self.data = pandas.DataFrame(results, columns=['sentiment', 'text'])
+        self.data['text_length'] = [len(text) for text in self.data['text']]
+
+    def load_data(self, category, directory, label):
+        join_path = os.path.join(self.data_directory, category, directory)
+
+        rows = []
+        with open(join_path) as file:
+            root = tree.fromstringlist(["<root>", file.read(), "</root>"], parser=self.parser)
+            for node in root:
+                review_node = node.find("review_text")
+                if review_node is not None:
+                    rows.append([label, review_node.text])
+        return rows
+
+    def list_dir(self):
+        result = os.listdir(self.data_directory)
+        result.remove('stopwords')
+        result.remove('summary.txt')
+        return result
