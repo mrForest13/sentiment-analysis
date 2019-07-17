d2f5c0a0ecd9209e59604ea6d77a745765c0e153
Mateusz Ligeza <m.ligeza@iceo.co>
2019-07-17 16:41:41 +0200

    fixed config

--- configuration/DatasetsConfig.py
+++ configuration/DatasetsConfig.py
@@ -1,3 +1,3 @@
-AIRLINE = "/home/mligeza/Downloads/airline/Tweets.csv"
-REVIEW = "/home/mligeza/Downloads/aclImdb/train"
-AMAZON = "/home/mligeza/Downloads/sorted_data"
+AIRLINE = "C:\\Users\\mateu\\Downloads\\airline\\Tweets.csv"
+REVIEW = "C:\\Users\\mateu\\Downloads\\aclImdb\\train"
+AMAZON = "C:\\Users\\mateu\\Downloads\\sorted_data"
